65e529c3c6031ec68f40cd2be56b729c7d8453d4
estudante Suelin Aiessa <106682937+suelinaiessa@users.noreply.github.com>
2024-06-10 09:09:44 -0300

    Add files via upload

--- lista.js
+++ lista.js
@@ -3,3 +3,13 @@ Document.querySelectorAll('#lista-itens li').forEach(function(item)
 document.getElementById ('mensagem-item'). text.Content = This.getAttribute
 ('data-mensagem'); });
 });
+document.querySelectorAll('#lista-itens li').forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.getElementById('mensagem-item').textContent = this.getAttribute
+        ('data-mensagem')
+        document.getElementById('imagem-item').src = this.getAttribute('data-imagem');
+        document.getElementById('imagem-item').style.display = 'block'; 
+        document.getElementById('descricao-item').textContent = this.getAttribute
+    ('data-descricao');
+});
+});
